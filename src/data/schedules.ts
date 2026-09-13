6544037fa17db50687ccb086304a0c0940796694
ju won bae <juwonbae@MacBook-Pro.local>
2026-09-14 05:49:31 +0900

    Update Pilgrimage start date to 10/19

--- src/data/schedules.ts
+++ src/data/schedules.ts
@@ -60,5 +60,5 @@ export const SCHEDULES: Schedule[] = [
   { id: 'r12', type: '기타 사역', startDate: '2026-09-18', endDate: '2026-09-18', participants: ['23'], location: '출장' },
   { id: 'r13', type: '기타 사역', startDate: '2026-10-09', endDate: '2026-10-09', participants: ['23'], location: '출장' },
   { id: 'r14', type: '기타 사역', startDate: '2026-10-23', endDate: '2026-10-23', participants: ['23'], location: '출장' },
-  { id: 'r15', type: '기타 사역', startDate: '2026-10-18', endDate: '2026-10-28', participants: ['24', '21', '3', '22'], location: '성지순례' },
+  { id: 'r15', type: '기타 사역', startDate: '2026-10-19', endDate: '2026-10-28', participants: ['24', '21', '3', '22'], location: '성지순례' },
 ];
